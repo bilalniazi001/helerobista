87fb33f3b4341bda14f45c61518f9209318a4612
bilalkhanniazi <bilalkhanniazi765@gmail.com>
2025-11-18 14:53:27 +0500

    final commit

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Yeh line Turbopack ko zabardasti band kar degi aur Webpack istemal hoga.
-  turbopack: {}, 
-     experimental: {
-     forceSwcTransforms: true, 
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // json-server aur next-connect ko external dependency bana diya
-      config.externals.push('json-server', 'next-connect');
-    }
-    return config;
-  },
-};
+  // Yahan koi experimental settings nahi hain
+  // Basic configuration rakhein agar zaroorat ho
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
